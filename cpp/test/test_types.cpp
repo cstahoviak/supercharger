a4b4cb6f0058ba9925ec31ec3015fb7c4365da89
cstahoviak <carlcstahoviak@gmail.com>
2024-11-21 14:11:56 -0700

    added separate unit test for PlannerResult copy ctor and copy assignment operator.

--- cpp/test/test_types.cpp
+++ cpp/test/test_types.cpp
@@ -17,16 +17,28 @@ using namespace supercharger;
 using namespace supercharger::algorithm;
 
 
-TEST(TestTypes, TestCopyPlannerResult)
+class TypesTestFixture : public testing::Test
 {
-  // Create the planner
-  std::unique_ptr<Planner> planner =
-    Planner::GetPlanner(Planner::CostFunctionType::DIJKSTRAS_SIMPLE);
+  protected:
+    void SetUp() override
+    {
+      // Suppress all terminal output
+      auto old_buffer = std::cout.rdbuf(nullptr);
 
-  // Plan the route
-  PlannerResult result = planner->Plan(
-    "Council_Bluffs_IA", "Cadillac_MI", 320, 105);
+      // Create the planner
+      std::unique_ptr<Planner> planner =
+      Planner::GetPlanner(Planner::CostFunctionType::DIJKSTRAS_SIMPLE);
 
+      // Plan the route
+      result = planner->Plan("Council_Bluffs_IA", "Cadillac_MI", 320, 105);
+    }
+
+    // Store the result of Dijkstra's planning algorithm.
+    PlannerResult result;
+};
+
+TEST_F(TypesTestFixture, TestNodeUniqueness)
+{
   // Verify that each node (managed by a shared_ptr) has a use_count of 1.
   for ( const std::shared_ptr<Node>& node_ptr : result.route ) {
     EXPECT_EQ(node_ptr.use_count(), 1);
@@ -36,10 +48,40 @@ TEST(TestTypes, TestCopyPlannerResult)
   for ( const std::shared_ptr<Node> node_ptr : result.route ) {
     EXPECT_EQ(node_ptr.use_count(), 2);
   }
+}
 
+TEST_F(TypesTestFixture, TestPlannerResultCopyConstructor)
+{
   // Copy the PlannerResult (uses the PlannerResult's copy ctor).
-  // PlannerResult copy(result);
-  
+  PlannerResult copy(result);
+
+  // Verify that top-level data was copied correctly.
+  EXPECT_EQ(copy.cost, result.cost);
+  EXPECT_EQ(copy.max_range, result.max_range);
+  EXPECT_EQ(copy.speed, result.speed);
+
+  // The copied PlannerResult should contain new nodes with identical info.
+  EXPECT_EQ(copy.route.size(), result.route.size());
+  for ( size_t idx = 0; idx < result.route.size(); idx++ ) {
+    // Expect inequality of pointers.
+    EXPECT_NE(copy.route[idx], result.route[idx]);
+
+    // Expect equality of all other route data.
+    EXPECT_EQ(copy.route[idx]->name(), result.route[idx]->name());
+    EXPECT_EQ(copy.route[idx]->duration, result.route[idx]->duration);
+    EXPECT_EQ(copy.route[idx]->arrival_range, result.route[idx]->arrival_range);
+    EXPECT_EQ(copy.route[idx]->departure_range, result.route[idx]->departure_range);
+  }
+
+  // Verify that the use count for each node is still 1.
+  for ( size_t idx = 0; idx < result.route.size(); idx++ ) {
+    EXPECT_EQ(result.route[idx].use_count(), 1);
+    EXPECT_EQ(copy.route[idx].use_count(), 1);
+  }
+}
+
+TEST_F(TypesTestFixture, TestPlannerResultCopyAssignment)
+{
   // Copy the PlannerResult (uses the PlannerResult's copy assignment operator).
   PlannerResult copy;
   copy = result;
